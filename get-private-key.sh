ccbc4552adae6e4b5f583f84ac491e03127c13e5
Ranbir Chawla <rchawla@thoughtworks.com>
2018-03-20 23:28:12 +0530

    copied key to ~/.ssh dir

--- get-private-key.sh
+++ get-private-key.sh
@@ -8,5 +8,6 @@ KEY_VALUE=$(aws ssm get-parameters --names $KEY_NAME --with-decryption --region 
 
 echo "$KEY_VALUE" > $FILE
 chmod 400 $FILE
+cp $FILE ~/.ssh/
 # add key to ssh management
 ssh-add $File
